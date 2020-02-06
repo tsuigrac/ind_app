27e44c2f14fadd33738686ced1279a27791b1fa0
tsuigrac <tsuigrac@grinnell.edu>
2020-02-06 16:22:53 -0600

    Remove duplication in HTML

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,18 +4,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home"
+    assert_select "title", "Home | Grinnell Software Development"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title" , "Help"
+    assert_select "title" , "Help | Grinnell Software Development"
   end
 
     test "should get about" do
         get static_pages_about_url
         assert_response:success
-        assert_select "title" , "About"
+        assert_select "title" , "About | Grinnell Software Development"
     end
 end
