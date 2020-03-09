6ba616347e4dc20933a29ca7143954caa866b963
tsuigrac <tsuigrac@grinnell.edu>
2020-03-08 19:37:05 -0500

    Display values from database

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,4 +1,16 @@
 class HabitsController < ApplicationController
+  def show
+      @phy_res = Habit.find(1)
+  end
+  
   def new
+      @phy_answers = Habit.where(category: "Physical")
+      @phy_result = @phy_answers[rand(@phy_answers.length)]
+      
+      @men_answers = Habit.where(category: "Mental")
+      @men_result = @men_answers[rand(@men_answers.length)]
+      
+      @soc_answers= Habit.where(category: "Socio")
+      @soc_result = @soc_answers[rand(@soc_answers.length)]
   end
 end
