223a40abd3bbd0b89d60ead7262f2ef64a27f02d
tsuigrac <tsuigrac@grinnell.edu>
2020-03-26 00:57:09 -0500

    Added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200308231805) do
+ActiveRecord::Schema.define(version: 20200326054851) do
 
   create_table "habits", force: :cascade do |t|
     t.string "category"
     t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
