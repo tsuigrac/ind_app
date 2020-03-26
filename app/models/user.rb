371059a33150e9d90f451a435c0b4b03982b8a0c
tsuigrac <tsuigrac@grinnell.edu>
2020-03-26 01:35:32 -0500

    check for duplicate emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+    #Save emails as lowercase
+    before_save {self.email = email.downcase}
+    
+    #Makes sure there is a valid name
     validates :name, presence: true, length: {maximum: 50}
     
+    #Makes sure email is in correct format
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
-                      format: { with: VALID_EMAIL_REGEX }
+                      format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: {case_sensitive: false}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200326054851) do
+ActiveRecord::Schema.define(version: 20200326062933) do
 
   create_table "habits", force: :cascade do |t|
     t.string "category"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20200326054851) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
